b73b1686d6a74856441a67bc52c685ccf2828d97
Dennis Tat <dtat24@gmail.com>
2025-06-26 15:57:19 -0700

    Add knex connection, migration, and seed

---
+++ api-easy-matters/knexfile.ts
@@ -0,0 +1,44 @@
+import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'postgres',
+    },
+    migrations: {
+      directory: './src/db/migrations',
+      extension: 'ts',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+      extension: 'ts',
+      loadExtensions: ['.ts'],
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'db',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'postgres',
+    },
+    migrations: {
+      directory: './dist/db/migrations',
+    },
+    seeds: {
+      directory: './dist/db/seeds',
+    },
+  },
+};
+
+export default config;
---
+++ api-easy-matters/src/db/migrations/20240626221600_create_users.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('email').notNullable();
+    table.string('firm_name').notNullable(),
+      table.unique(['email', 'firm_name']);
+    table.string('password_hash').notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
