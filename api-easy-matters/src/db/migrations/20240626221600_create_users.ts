588613fccb472fd9c4d9b56e497d078da28c5423
Dennis Tat <dtat24@gmail.com>
2025-06-26 19:22:27 -0700

    Add auth/me endpoint and update login to only use email and pw

--- api-easy-matters/src/db/migrations/20240626221600_create_users.ts
+++ api-easy-matters/src/db/migrations/20240626221600_create_users.ts
@@ -3,9 +3,8 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
-    table.string('email').notNullable();
+    table.string('email').notNullable().unique();
     table.string('firm_name').notNullable();
-    table.unique(['email', 'firm_name']);
     table.string('password_hash').notNullable();
     table.timestamps(true, true);
   });
