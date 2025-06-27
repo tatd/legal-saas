6e34d14e335d6e0fd73bf8d7e9d6e723fac3687a
Dennis Tat <dtat24@gmail.com>
2025-06-27 09:45:14 -0700

    Implement shadcn

--- easy-matters/src/App.tsx
+++ easy-matters/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -45,6 +46,9 @@ function App() {
       <p className="text-sm text-gray-500">
         Click on the Vite and React logos to learn more!!!
       </p>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
